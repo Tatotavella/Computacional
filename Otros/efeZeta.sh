b3ff4ca4a0e0159d7df7b6b5c62044886cb361ae
Franco Tavella <tavellafran@gmail.com>
2017-05-01 13:53:07 -0300

    Scripts para cada punto por separado

--- Otros/efeZeta.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-LANG=en_US
-for i in $(seq 0 0.05 1)
-do
-	./efezeta.e 64 64 $i
-	echo $i 
-done
--- Otros/gMatch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-LANG=en_US
-for i in $(seq 0 0.02 1)
-do
-	./gammamatch.e 128 128 $i
-	echo $i 
-done
